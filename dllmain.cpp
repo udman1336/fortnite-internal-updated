6e560fec7855dbfb087353fc981c67b796ae6439
ud man <141735200+udman1336@users.noreply.github.com>
2024-02-04 21:35:22 -0330

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,11 +1,9 @@
 #include <includes.h>
 
-std::future <void> main_future;
-
 void DllStart()
 {
     GObjects = GetObjects();
-    printf("GObjects!\n");
+    printf("GObjects Found!\n");
 
     KismetMathLibrary = UObject::FindObject(L"Engine.Default__KismetMathLibrary");
     GameplayStatics = UObject::FindObject(L"Engine.Default__GameplayStatics");
@@ -13,7 +11,7 @@ void DllStart()
     FortKismetLibrary = UObject::FindObject(L"FortniteGame.Default__FortKismetLibrary");
     KismetSystemLibrary = UObject::FindObject(L"Engine.Default__KismetSystemLibrary");
     KismetStringLibrary = UObject::FindObject(L"Engine.Default__KismetStringLibrary");
-    printf("Libraries!\n");
+    printf("Libraries Are Good!\n");
 
     Keys::LeftMouseButton = FKey{ FName{ UKismetStringLibrary::Conv_StringToName(L"LeftMouseButton") }, 0 };
     Keys::RightMouseButton = FKey{ FName{ UKismetStringLibrary::Conv_StringToName(L"RightMouseButton") }, 0 };
@@ -33,11 +31,11 @@ void DllStart()
     Variables::MaterialParameter5 = UKismetStringLibrary::Conv_StringToName(L"Emissive_Color");
 
     UWorld* GWorld = UWorld::GetWorld();
-    printf("GWorld: %p\n", GWorld);
+    printf("UWorld: %p\n", GWorld);
     UGameInstance* OwningGameInstance = GWorld->OwningGameInstance();
     ULocalPlayer* LocalPlayer = OwningGameInstance->LocalPlayers()[0];
     UGameViewportClient* ViewportClient = LocalPlayer->ViewportClient();
-    printf("ViewportClient: %p\n", ViewportClient);
+    printf("UGameViewportClient: %p\n", ViewportClient);
 
     // CanvasObject -> temporary canvas if there isn't already one
     //Variables::Canvas = reinterpret_cast<UCanvas*>(UObject::FindObject(L"DebugCanvasObject", reinterpret_cast<UObject*>(-1)));
@@ -62,18 +60,15 @@ BOOL DllMain(void* hModule, DWORD ul_reason_for_call, void* lpReserved)
         freopen("conin$", "r", stdin);
         freopen("conout$", "w", stdout);
         freopen("conout$", "w", stderr);
-        printf("Injected!\n");
-        Sleep(500);
 
         Variables::BuildTime = GetBuildTime();
 
         auto EncryptedModule_FortniteClientWin64Shipping = Encrypt(L"FortniteClient-Win64-Shipping.exe");
         GameBase = FindModuleByName(EncryptedModule_FortniteClientWin64Shipping.decrypt()); EncryptedModule_FortniteClientWin64Shipping.clear();
         if (!GameBase) return false;
-        printf("GameBase: %p\n", GameBase);
+        printf("Base Address: %p\n", GameBase);
 
         SpoofCall::Initialize(reinterpret_cast<unsigned char*>(GameBase));
-        printf("Initialized SpoofCall\n");
 
         reinterpret_cast<void(__cdecl*)(uintptr_t, void*)>(DoSpoofCall)(SpoofCode, &DllStart);
     }
